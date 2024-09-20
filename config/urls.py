856d493f1937c75e01812871b8d021a23763f164
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-09-20 22:33:16 +0500

    change settings

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,32 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include
+from django.conf.urls.static import static
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path, re_path
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="learning drf",
+        default_version='v1',
+        description="learning drf from web site ",
+        # terms_of_service="https://www.google.com/policies/terms/",
+        # contact=openapi.Contact(email="your_email@domain.com"),
+        # license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  # Swagger uchun URL'larni qo'shish
+                  path('admin/', admin.site.urls),
+                  re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
+                          name='schema-json'),
+                  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+                  path("api/v1/", include('src.core.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
